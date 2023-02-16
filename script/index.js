89c3f6d4c83d760b94b4d38d68ed1e7f6783fb3e
Eduard Ivankovich <eduard.ivankovich@yandex.ru>
2023-02-16 17:11:34 +0300

    bugfix: исправление в соответствии с комментариями

--- script/index.js
+++ script/index.js
@@ -1,42 +1,30 @@
 const editButton = document.querySelector('.profile__edit-button');
 const popup = document.querySelector('.popup');
 const closePopup = document.querySelector('.popup__close');
-const buttonConfirm = document.querySelector('.popup__confirm');
 
-let formElement = document.querySelector('.popup__container');
+let formElement = document.querySelector('.popup__form');
 let nameLabel = document.querySelector('.profile__name');
 let descriptionLabel = document.querySelector('.profile__description');
-let nameChange = document.querySelector('.popup__name-change');
-let descriptionChange = document.querySelector('.popup__description-change');
-
-nameChange.value = nameLabel.textContent;
-descriptionChange.value = descriptionLabel.textContent;
+let nameChange = document.querySelector('.popup__inputs:first-of-type');
+let descriptionChange = document.querySelector('.popup__inputs:last-of-type');
 
 const togglePopupEdit = () => {
     popup.classList.toggle('popup_opened');
 }
 
 const handleEditButtonClick = () => {
-    togglePopupEdit();
-}
-
-const handleCloseButtonClick = () => {
+    nameChange.value = nameLabel.textContent;
+    descriptionChange.value = descriptionLabel.textContent;
     togglePopupEdit();
 }
 
 function handleFormSubmit(evt) {
-    evt.preventDefault();
-}
-
-const handleConfirmButtonClick = () => {
     togglePopupEdit();
-    nameChange.getAttribute('value');
-    descriptionChange.getAttribute('value');
     nameLabel.textContent = nameChange.value;
     descriptionLabel.textContent = descriptionChange.value;
+    evt.preventDefault();
 }
 
 formElement.addEventListener('submit', handleFormSubmit);
 editButton.addEventListener('click', handleEditButtonClick);
-closePopup.addEventListener('click', handleCloseButtonClick);
-buttonConfirm.addEventListener('click', handleConfirmButtonClick)
+closePopup.addEventListener('click', togglePopupEdit);
