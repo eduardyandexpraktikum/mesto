a6c3862d01a65d5259f5452d2dedba278e4b8ca7
Eduard Ivankovich <eduard.ivankovich@yandex.ru>
2023-04-15 13:58:52 +0300

    бэкап на всякий

--- script/PopupWithImage.js
+++ script/PopupWithImage.js
@@ -3,5 +3,14 @@ import { Popup } from "./Popup.js";
 export class PopupWithImage extends Popup {
     constructor(selector) {
         super(selector)
+        this._popupImageLink = this._selector.querySelector('.popupImage__pic')
+        this._popupImageName = this._selector.querySelector('.popupImage__description')
+    }
+
+    open(link, name) {
+        super.open();
+        this._popupImageLink = link;
+        this._popupImageName = name;
+        this._popupImageName.alt = name;
     }
 }
